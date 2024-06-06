4aa88e7227a20db7d6a25c5bc150ef804aa759a5
Kavishka Thushal <128395749+KavishkaThushal@users.noreply.github.com>
2024-06-06 10:24:17 +0530

    bug fix

--- Server/index.js
+++ Server/index.js
@@ -19,6 +19,7 @@ app.use(cors())
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+
 mongoose.connect(process.env.URL).then(()=>{
     console.log('database connected successfully..')
   }).catch((err)=>{
